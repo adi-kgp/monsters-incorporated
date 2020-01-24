4e99d81469868d87499ffc589b1ff20f583f88d1
adi-kgp <adi.kgp@gmail.com>
2020-01-24 20:30:01 +0530

    Updates

---
+++ precache-manifest.8977edbf9a63b1db447957ea67906868.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c92e54f25367a14a1b51b574f7690af4",
+    "url": "/monsters-incorporated/index.html"
+  },
+  {
+    "revision": "7a748f5c0143dd12b039",
+    "url": "/monsters-incorporated/static/css/main.eef36ab8.chunk.css"
+  },
+  {
+    "revision": "4c43680a4c1defaef845",
+    "url": "/monsters-incorporated/static/js/2.ec8ba6cb.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/monsters-incorporated/static/js/2.ec8ba6cb.chunk.js.LICENSE"
+  },
+  {
+    "revision": "7a748f5c0143dd12b039",
+    "url": "/monsters-incorporated/static/js/main.d862a23c.chunk.js"
+  },
+  {
+    "revision": "96400f91fdd8205f686e",
+    "url": "/monsters-incorporated/static/js/runtime-main.13c171af.js"
+  }
+]);
